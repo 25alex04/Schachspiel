e6cc2e7e477b72882529e69fbabee7128d194a58
25alex04 <57731896+25alex04@users.noreply.github.com>
2021-05-27 12:30:01 +0200

    ImageViews und Piece Bilder

--- app/src/main/java/de/haw_hamburg/schachspiel/GameView.java
+++ app/src/main/java/de/haw_hamburg/schachspiel/GameView.java
@@ -30,11 +30,87 @@ public class GameView extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        setContentView(R.layout.gameview);
 
-        TextView textView = findViewById(R.id.testclickview);
 
-        textView.setOnClickListener(this::onClick);
+        TextView A1 = findViewById(R.id.A1);
+        TextView A2 = findViewById(R.id.A2);
+        TextView A3 = findViewById(R.id.A3);
+        TextView A4 = findViewById(R.id.A4);
+        TextView A5 = findViewById(R.id.A5);
+        TextView A6 = findViewById(R.id.A6);
+        TextView A7 = findViewById(R.id.A7);
+        TextView A8 = findViewById(R.id.A8);
+        TextView B1 = findViewById(R.id.B1);
+        TextView B2 = findViewById(R.id.B2);
+        TextView B3 = findViewById(R.id.B3);
+        TextView B4 = findViewById(R.id.b4);
+        TextView B5 = findViewById(R.id.B5);
+        TextView B6 = findViewById(R.id.B6);
+        TextView B7 = findViewById(R.id.B7);
+        TextView B8 = findViewById(R.id.B8);
+        TextView C1 = findViewById(R.id.C1);
+        TextView C2 = findViewById(R.id.C2);
+        TextView C3 = findViewById(R.id.C3);
+        TextView C4 = findViewById(R.id.C4);
+        TextView C5 = findViewById(R.id.C5);
+        TextView C6 = findViewById(R.id.C6);
+        TextView C7 = findViewById(R.id.C7);
+        TextView C8 = findViewById(R.id.C8);
+        TextView D1 = findViewById(R.id.D1);
+        TextView D2 = findViewById(R.id.D2);
+        TextView D3 = findViewById(R.id.D3);
+        TextView D4 = findViewById(R.id.D4);
+        TextView D5 = findViewById(R.id.D5);
+        TextView D6 = findViewById(R.id.D6);
+        TextView D7 = findViewById(R.id.D7);
+        TextView D8 = findViewById(R.id.D8);
+        TextView E1 = findViewById(R.id.E1);
+        TextView E2 = findViewById(R.id.E2);
+        TextView E3 = findViewById(R.id.E3);
+        TextView E4 = findViewById(R.id.E4);
+        TextView E5 = findViewById(R.id.E5);
+        TextView E6 = findViewById(R.id.E6);
+        TextView E7 = findViewById(R.id.E7);
+        TextView E8 = findViewById(R.id.E8);
+        TextView F1 = findViewById(R.id.F1);
+        TextView F2 = findViewById(R.id.F2);
+        TextView F3 = findViewById(R.id.F3);
+        TextView F4 = findViewById(R.id.F4);
+        TextView F5 = findViewById(R.id.F5);
+        TextView F6 = findViewById(R.id.F6);
+        TextView F7 = findViewById(R.id.F7);
+        TextView F8 = findViewById(R.id.F8);
+        TextView G1 = findViewById(R.id.G1);
+        TextView G2 = findViewById(R.id.G2);
+        TextView G3 = findViewById(R.id.G3);
+        TextView G4 = findViewById(R.id.G4);
+        TextView G5 = findViewById(R.id.G5);
+        TextView G6 = findViewById(R.id.G6);
+        TextView G7 = findViewById(R.id.G7);
+        TextView G8 = findViewById(R.id.G8);
+        TextView H1 = findViewById(R.id.H1);
+        TextView H2 = findViewById(R.id.H2);
+        TextView H3 = findViewById(R.id.H3);
+        TextView H4 = findViewById(R.id.H4);
+        TextView H5 = findViewById(R.id.H5);
+        TextView H6 = findViewById(R.id.H6);
+        TextView H7 = findViewById(R.id.H7);
+        TextView H8 = findViewById(R.id.H8);
+
+        ImageView bl_bauer1 = findViewById(R.id.bl_bauer1);
+        ImageView bl_bauer2 = findViewById(R.id.bl_bauer2);
+        ImageView bl_bauer3 = findViewById(R.id.bl_bauer3);
+        ImageView bl_bauer4 = findViewById(R.id.bl_bauer4);
+        ImageView bl_bauer5 = findViewById(R.id.bl_bauer5);
+        ImageView bl_bauer6 = findViewById(R.id.bl_bauer6);
+        ImageView bl_bauer7 = findViewById(R.id.bl_bauer7);
+        ImageView bl_bauer8 = findViewById(R.id.bl_bauer8);
+
+
+
+
+
 
 
         //bestaetigenP1 = findViewById(R.id.zugBestätigenButton);
